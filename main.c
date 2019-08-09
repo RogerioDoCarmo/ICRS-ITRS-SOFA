07b37fd5100a9ad530be351f5c672ec5eb22bfc4
Rogério do Carmo <rogerio.carmo02@gmail.com>
2019-08-09 15:34:11 -0300

    Added SOFA librayry

--- main.c
+++ main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "lib/sofa/sofa.h"
+
 int main()
 {
-    printf("Hello world!\n");
+    int YEAR, MONTH, DAY_MONTH;
+    double mjd0, mjd;
+
+    YEAR        = 2019;
+    MONTH       = 8;
+    DAY_MONTH   = 7;
+
+    iauCal2jd(YEAR,MONTH,DAY_MONTH, &mjd0, &mjd);
+
+    printf("%f",mjd);
+
+
+    getchar();
+
     return 0;
 }
